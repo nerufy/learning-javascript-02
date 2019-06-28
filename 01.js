f6df1b8c6bc407851ddb5eba3f7b0a6067ea149d
nerufy <nerufy1031@gmail.com>
2019-06-28 10:00:03 +0900

    6/28 課題

--- 01.js
+++ 01.js
@@ -1,10 +1,20 @@
 // FizzBuzz
 
 // サンプル: "1" という文字列を出力
-console.log("1");
+for(var i=1; i <=15; i++ ){
+    if(!(i%3)&&(i%5)){
+        process.stdout.write("Fizz");
+        console.log("")
+    }else if(!(i%5)&&(i%3)){
+        process.stdout.write("Buzz");
+        console.log("")
+    }else if(!(i%15)){
+        process.stdout.write("FizzBuzz");
+        console.log("")
+    }else{
+        console.log(i);
+    }
+}
 
 // サンプル: "FizzBuzz" という文字列を出力
 // (process.stdout.write で改行なしの出力ができる)
-process.stdout.write("Fizz");
-process.stdout.write("Buzz");
-console.log();
